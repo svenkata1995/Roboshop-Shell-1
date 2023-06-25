72cf7cc2a654b99ede4941d44112332d9d86e0ae
svenk <svenkata1995@gmail.com>
2023-06-25 17:19:30 +0530

    given full path

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,3 +1,4 @@
+  echo -e "\e[34m>>>>>>>>>>>> Configuring NodeJs repos <<<<<<<<<<\e[om"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
   yum install nodejs -y
   useradd roboshop
@@ -7,10 +8,10 @@
   unzip /tmp/catalogue.zip
   cd /app
   npm install
-  cp catalogue.service /etc/systemd/system/catalogue.service
+  cp /home/centos/Roboshop-Shell-1/catalogue.service /etc/systemd/system/catalogue.service
   systemctl daemon-reload
   systemctl enable catalogue
   systemctl start catalogue
-  cp mongo1.repo /etc/yum.repos.d/mongo.repo
+  cp /home/centos/Roboshop-Shell-1/mongo1.repo /etc/yum.repos.d/mongo.repo
   yum install mongodb-org-shell -y
   mongo --host mongodb-dev.rdevopsb72.tech </app/schema/catalogue.js
