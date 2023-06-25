985921d5f9d8e3c924b79a7c1e0dcb4eae5b0112
svenk <svenkata1995@gmail.com>
2023-06-25 15:54:27 +0530

    MongoDB script with conf file

---
+++ MongoDB.sh
@@ -0,0 +1,6 @@
+cp Mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+systemctl restart mongod
