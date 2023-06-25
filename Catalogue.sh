5a84c04f112d1e6ef83ea83a20052f19c5a08c10
svenk <svenkata1995@gmail.com>
2023-06-25 17:23:42 +0530

    given full path

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,7 +1,8 @@
-  echo -e "\e[34m>>>>>>>>>>>> Configuring NodeJs repos <<<<<<<<<<\e[om"
+  echo -e "\e[34m>>>>>>>>>>>> Configuring NodeJs repos <<<<<<<<<<\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
   yum install nodejs -y
   useradd roboshop
+  rm -rf /app
   mkdir /app
   curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
   cd /app
